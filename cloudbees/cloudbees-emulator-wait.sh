0a9aef775aa361299c63b2e78449310f90a261d7
Stefan Miklosovic <smikloso@redhat.com>
2013-07-18 02:06:41 +0200

    added adb command not on cloudbees PATH

--- cloudbees/cloudbees-emulator-wait.sh
+++ cloudbees/cloudbees-emulator-wait.sh
@@ -9,8 +9,10 @@
 
 AVD_NAME=test01
 MEMORY=256
+ADB_CMD=/opt/android/android-sdk-linux/platform-tools/adb
 
 echo "Starting emulator $AVD_NAME"
+echo "executing: 'emulator -avd $AVD_NAME -no-audio -no-window -memory $MEMORY -nocache -no-snapshot-save -no-snapstorage'"
 
 emulator -avd $AVD_NAME -no-audio -no-window -memory $MEMORY -nocache -no-snapshot-save -no-snapstorage &
 
@@ -19,7 +21,7 @@ echo "Waiting until emulator $AVD_NAME is booted"
 booted=""
 until [[ "$booted" =~ "stopped" ]]; do
     # -e directs command to the only running emulator
-    booted=`adb -e shell getprop init.svc.bootanim`
+    booted=`$ADB_CMD -e shell getprop init.svc.bootanim`
     echo -n $booted
     if [[ "$booted" =~ "not found" ]]; then
         echo "Could not boot emulator"
