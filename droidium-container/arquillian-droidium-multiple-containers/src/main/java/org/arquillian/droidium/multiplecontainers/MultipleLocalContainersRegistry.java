6c0177fd0c8008ebd4d6adc88a8b681fec3e9f7e
Stefan Miklosovic <smikloso@redhat.com>
2013-10-05 21:26:39 +0200

    ARQ-1513 Multicontainer extension should not log warnings about its property

--- droidium-container/arquillian-droidium-multiple-containers/src/main/java/org/arquillian/droidium/multiplecontainers/MultipleLocalContainersRegistry.java
+++ droidium-container/arquillian-droidium-multiple-containers/src/main/java/org/arquillian/droidium/multiplecontainers/MultipleLocalContainersRegistry.java
@@ -44,9 +44,9 @@ import org.jboss.arquillian.core.spi.Validate;
 /**
  * This class registers all adapters which are specified in the arquillian.xml.
  *
- * In the case there is only one adapter implementation on the classpath, it is not necessary to specify it in the
- * container configuration since it will be used automatically. You have to specify it only in the case you are going to
- * use more than one container.
+ * In the case there is only one adapter implementation on the classpath, it is not necessary to specify it in the container
+ * configuration since it will be used automatically. You have to specify it only in the case you are going to use more than one
+ * container.
  *
  * @author Dominik Pospisil <dpospisi@redhat.com>
  * @author Stefan Miklosovic <smikloso@redhat.com>
@@ -83,20 +83,44 @@ public class MultipleLocalContainersRegistry implements ContainerRegistry {
             }
 
             if (services.size() == 1) {
+                // just one container on cp
                 dcService = services.iterator().next();
             } else {
-                Map<String, String> props = definition.getContainerProperties();
-                if (!props.containsKey("adapterImplClass")) {
-                    throw new ConfigurationException("Container adapter implementation class must be provided via "
-                            + ADAPTER_IMPL_CONFIG_STRING + " property.");
+                dcService = guessDeployableContainer(definition, services);
+
+                // there are two containers but we failed to guess the current one, we are
+                // pretty sure it is not android or any one we can guess
+                if (dcService == null && services.size() == 2 && isAndroidContainerRegistered()) {
+                    for (DeployableContainer<?> service : services) {
+                        if (!service.getClass().getName().equals("org.arquillian.droidium.container.AndroidDeployableContainer")) {
+                            dcService = service;
+                        }
+                    }
                 }
 
-                Class<?> dcImplClass = Class.forName(props.get(ADAPTER_IMPL_CONFIG_STRING));
+                // >= 2 containers and none of them is Android, we just stick to adapterImplClass property
+                if (dcService == null) {
+                    Map<String, String> props = definition.getContainerProperties();
+                    if (!props.containsKey(ADAPTER_IMPL_CONFIG_STRING)) {
+                        logger.log(Level.WARNING, "Unable to get container adapter class for container with "
+                            + "qualifier {0}. It is expected that you pass {1} property with class name which implements "
+                            + "DeployableContainer interface for given container definition. It is not necessary to "
+                            + "specify adapterImplClass property in case your container qualifier name in arquillian.xml, "
+                            + "after lowercasing, contains or is equal to string: android, jboss, glassfish, tomee, openshift. "
+                            + "It is expected that when you name your container like that, you put Arquillian container "
+                            + "adapter for that container on the classpath.",
+                            new Object[] { definition.getContainerName(), ADAPTER_IMPL_CONFIG_STRING });
+                        throw new ConfigurationException("Container adapter implementation class must be provided via "
+                            + ADAPTER_IMPL_CONFIG_STRING + " property.");
+                    }
 
-                for (DeployableContainer<?> dc : services) {
-                    if (dcImplClass.isInstance(dc)) {
-                        dcService = dc;
-                        break;
+                    Class<?> dcImplClass = Class.forName(props.get(ADAPTER_IMPL_CONFIG_STRING));
+
+                    for (DeployableContainer<?> dc : services) {
+                        if (dcImplClass.isInstance(dc)) {
+                            dcService = dc;
+                            break;
+                        }
                     }
                 }
             }
@@ -112,9 +136,50 @@ public class MultipleLocalContainersRegistry implements ContainerRegistry {
         }
     }
 
-    @Override
-    public Container getContainer(String name) {
-        return findMatchingContainer(name);
+    @SuppressWarnings("rawtypes")
+    private DeployableContainer<?> guessDeployableContainer(ContainerDef definition, Collection<DeployableContainer> services) {
+
+        Map<String, String> properties = definition.getContainerProperties();
+        String containerName = definition.getContainerName().toLowerCase();
+        DeployableContainer<?> container = null;
+
+        if (containerName.contains("android")
+            || properties.containsKey("avdName")
+            || properties.containsKey("serialId")
+            || properties.containsKey("consolePort")
+            || properties.containsKey("emulatorOptions")) {
+            container = parseContainer("org.arquillian.droidium.container.AndroidDeployableContainer", services);
+        } else if (containerName.contains("jboss")) {
+            container = parseContainer("org.jboss.as.arquillian.container", services);
+        } else if (containerName.contains("glassfish")) {
+            container = parseContainer("org.jboss.arquillian.container.glassfish", services);
+        } else if (containerName.contains("tomee")) {
+            container = parseContainer("org.apache.openejb.arquillian", services);
+        } else if (containerName.contains("openshift")) {
+            container = parseContainer("org.jboss.arquillian.container.openshift", services);
+        }
+
+        return container;
+    }
+
+    @SuppressWarnings("rawtypes")
+    private DeployableContainer<?> parseContainer(String className, Collection<DeployableContainer> services) {
+        for (DeployableContainer<?> container : services) {
+            if (container.getClass().getName().startsWith(className)) {
+                return container;
+            }
+        }
+        return null;
+    }
+
+    private boolean isAndroidContainerRegistered() {
+        for (Container container : containers) {
+            if (container.getDeployableContainer().getConfigurationClass().getName().equals(
+                "org.arquillian.droidium.container.configuration.AndroidContainerConfiguration")) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -155,6 +220,11 @@ public class MultipleLocalContainersRegistry implements ContainerRegistry {
             }
         }
         return null;
+    }
+
+    @Override
+    public Container getContainer(String name) {
+        return findMatchingContainer(name);
     }
 
 }
