ede4257218a162f4e8471a4bc125d087a22cb0e3
Stefan Miklosovic <smikloso@redhat.com>
2013-11-19 23:51:38 +0100

    refactored multiple-deployments-01 test

--- tests/droidium-multiple-deployments-01/src/test/java/org/arquillian/droidium/showcase/native_/test01/SelendroidTestAppTestCase.java
+++ tests/droidium-multiple-deployments-01/src/test/java/org/arquillian/droidium/showcase/native_/test01/SelendroidTestAppTestCase.java
@@ -50,6 +50,11 @@ import org.openqa.selenium.WebElement;
  *
  * Deployment which is meant to be instrumented has its {@link Deployment} method annotated with {@link Instrumentable}.
  *
+ * <br>
+ * <br>
+ *
+ * This test does not use page fragment abstraction from Graphene. You can see page fragments in action in hybrid-01 test.
+ *
  * @author <a href="mailto:smikloso@redhat.com">Stefan Miklosovic</a>
  *
  */
@@ -57,15 +62,18 @@ import org.openqa.selenium.WebElement;
 @RunAsClient
 public class SelendroidTestAppTestCase {
 
-    @Instrumentable // tells that this deployment will be instrumented by Arquillian Droidium
-    @Deployment(name = "selendroid") // name is the must, you can not deploy two deployments with the same default name
-    @TargetsContainer("android") // does not have to be here since we have just one container present
+    @Instrumentable
+    // tells that this deployment will be instrumented by Arquillian Droidium
+    @Deployment(name = "selendroid")
+    // name is the must, you can not deploy two deployments with the same default name
+    @TargetsContainer("android")
+    // does not have to be here since we have just one container present
     public static Archive<?> createSelendroidDeployment() {
         return ShrinkWrap.createFromZipFile(JavaArchive.class, new File("selendroid-test-app-0.5.1.apk"));
     }
 
+    // second deployment, it does not matter which one it is, shown here just for completeness, this apk is not used in the test
     @Deployment(name = "aerogear")
-    @TargetsContainer("android")
     public static Archive<?> createAeroGearDeployment2() {
         return ShrinkWrap.createFromZipFile(JavaArchive.class, new File("aerogear-test-android.apk"));
     }
@@ -74,7 +82,7 @@ public class SelendroidTestAppTestCase {
      * Simple test which tries to register some user.
      *
      * @param android Android device itself, it is not needed in tests as such since we interact only with {@code WebDriver}
-     *        injection.
+     *        injection. It is shown here only for completeness.
      * @param driver {@code WebDriver} injection which sends commands to Selendroid server installed on the Android device.
      */
     @Test
