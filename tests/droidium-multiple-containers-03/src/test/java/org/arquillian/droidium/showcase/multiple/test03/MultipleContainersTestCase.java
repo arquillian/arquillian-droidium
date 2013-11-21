074e45d73e7dcba9405bc6b0f6826bc0a5c5a98c
Stefan Miklosovic <smikloso@redhat.com>
2013-11-21 17:53:37 +0100

    Added example test project for ARQ-1577

---
+++ tests/droidium-multiple-containers-03/src/test/java/org/arquillian/droidium/showcase/multiple/test03/MultipleContainersTestCase.java
@@ -0,0 +1,56 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.droidium.showcase.multiple.test03;
+
+import org.arquillian.droidium.showcase.classes.Foo;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Proof of concept test for showing multiple containers on classpath.
+ *
+ * @author <a href="mailto:smikloso@redhat.com">Stefan Miklosovic</a>
+ *
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class MultipleContainersTestCase {
+
+    // this asks for firefox browser
+    @Drone
+    WebDriver browser;
+
+    @Deployment(name = "jbossas") // only one container will be started so we can omit @TargetsContainer
+    public static Archive<?> createJBossASDeployment() {
+        return ShrinkWrap.create(JavaArchive.class, "jbossas.jar").addClass(Foo.class);
+    }
+
+    @Test
+    public void test01() {
+        Assert.assertNotNull(browser);
+    }
+
+}
